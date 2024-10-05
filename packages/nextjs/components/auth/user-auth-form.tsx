7410e75588e87076a1af47612a2bd6eb2ccb5534
zxcvbinz <davide.lanotte.unix@gmail.com>
2024-10-05 07:00:51 +0200

    Refactor page component to add a test page

--- packages/nextjs/components/auth/user-auth-form.tsx
+++ packages/nextjs/components/auth/user-auth-form.tsx
@@ -12,21 +12,9 @@ import { GlobeLock, Loader2 } from "lucide-react";
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export default function UserAuthForm({ className, ...props }: UserAuthFormProps) {
-  //   const [isLoading, setIsLoading] = React.useState<boolean>(false);
-
-  //   async function onSubmit(event: React.SyntheticEvent) {
-  //     event.preventDefault();
-  //     setIsLoading(true);
-
-  //     setTimeout(() => {
-  //       setIsLoading(false);
-  //     }, 3000);
-  //   }
-
   return (
     <div className={cn("grid gap-6", className)} {...props}>
       <ConnectButtonCustom />
-      <DisconnectButton />
     </div>
   );
 }
