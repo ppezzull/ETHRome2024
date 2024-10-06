22663e8724f52b6665ff4844462dea5f3a795efc
zxcvbinz <davide.lanotte.unix@gmail.com>
2024-10-06 08:03:56 +0200

    Remove unused downloadData page and component

--- packages/nextjs/app/downloadData/page.tsx
+++
@@ -1,9 +0,0 @@
-import DownloadData from "@/components/downloadData/download-data";
-
-export default function Page() {
-  return (
-    <div className="flex min-h-svh justify-center items-center">
-      <DownloadData />
-    </div>
-  );
-}
